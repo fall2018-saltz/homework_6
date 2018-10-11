fcb758ca7755b0401d4ca2df325ca3f5218a49f2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 20:58:16 -0400

    Saving scatter.R

--- scatter.R
+++ scatter.R
@@ -1,1 +1,5 @@
 
+library("ggplot2")
+pop_stat_scatter <- ggplot(combined_df, aes(x=population, y = percentOver18))
+pop_stat_scatter <- pop_stat_scatter + geom_point(aes(size=TotalStateMurders, color = TotalStateMurders))
+pop_stat_scatter
