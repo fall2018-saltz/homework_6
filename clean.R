7ac16ab630c5350d4230b61516f3a6ec15f5d012
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 20:33:32 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,34 @@
 
-pop_df
+dfStates <- read.csv(url(urlread),stringsAsFactors = FALSE) ##call local sheet
+str(dfStates)
+dfStates
+
+arrests <- USArrests 
+stateName <- rownames(arrests)
+rownames(arrests) <- NULL
+arrests <- cbind(arrests,stateName)
+View(arrests)
+
+##CLEANING DATA
+dfStates <- dfStates[-53,]
+dfStates <- dfStates[-1,]
+dfStates <- dfStates[,-1:-4]
+View(dfStates)
+##Making sure there are exactly 51 rows
+nrow(dfStates)
+filter_df <- dfStates
+##Making 4 colums with stateName, population, popOver18, and percentOver18
+cnames <- colnames(dfStates)
+cnames[1] <- "stateName"
+cnames[2] <- "population"
+cnames[3] <- "popOver18"
+cnames[4] <- "percentOver18"
+colnames(dfStates) <- cnames
+colnames(dfStates)
+
+##Step B: explore data, understanding distributions
+##arrests <- arrests %>% rownames_to_column("stateName")
+common_col_names <- intersect(names(arrests), names(dfStates))
+combined_df <- merge.data.frame(arrests, dfStates, by = common_col_names, all.x = TRUE)
+View(combined_df)
+
